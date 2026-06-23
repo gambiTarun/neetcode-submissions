167bff7e2d9e8057fdcd45daa52d4ba767b9e175
Tarunbir Gambhir <tgambhir98@gmail.com>
2026-06-23 00:27:22 -0700

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.cpp
@@ -0,0 +1,43 @@
+class DSU {
+public:
+    vector<int> parent;
+    vector<int> rank;
+
+    DSU(int n) : parent(n), rank(n, 1) {
+        std::iota(parent.begin(), parent.end(), 0);
+    }
+
+    int findp(int x){
+        while(x!=parent[x]){
+            x = parent[x];
+        }
+        return x;
+    }
+
+    bool union_(int a, int b){
+        int x = findp(a);
+        int y = findp(b);
+        if(x==y)
+            return false;
+        if(rank[x] < rank[y])
+            swap(x, y);
+
+        parent[y] = x;
+        rank[x] += rank[y];
+        rank[y] += rank[x];
+        return true;
+    }
+};
+
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        DSU dsu(n);
+        int res = n;
+        for(auto &e: edges){
+            if(dsu.union_(e[0], e[1]))
+                res--;
+        }
+        return res;
+    }
+};
